913fa9d1d7e0a3512d0da1e20eae21be427808cf
Myat Min Aung <iridiamthegreat@gmail.com>
2023-04-28 23:44:55 +0630

    fix: api constants

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,17 +2,10 @@ export const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 export const apiKEY = process.env.NEXT_PUBLIC_API_KEY;
 
 // games
-export const GET_GAMES_GENRES = `${baseURL}/games`;
-
-export const GET_PLATFORMS_GAMES = `${baseURL}/games`;
+export const GET_GAMES = `${baseURL}/games`;
 
 // genres
 export const GET_GENRES = `${baseURL}/genres`;
 
-export const GET_GENRES_DETAILS = `${baseURL}/genres`;
-
-
 // platforms
 export const GET_PLATFORMS = `${baseURL}/platforms`;
-
-export const GET_PLATFORMS_DETAILS = `${baseURL}/platforms`
