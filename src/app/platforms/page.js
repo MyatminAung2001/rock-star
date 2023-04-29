594fc13bad0426e4ef8ae738d0e032de72929d61
Myat Min Aung <iridiamthegreat@gmail.com>
2023-04-29 10:46:01 +0630

    rename: components

--- src/app/platforms/page.js
+++ src/app/platforms/page.js
@@ -4,33 +4,39 @@ import { useRouter } from "next/navigation";
 import { useQuery } from "react-query";
 
 import { getPlatforms } from "@/services/service.platform";
-import Card from "@/components/card";
+import Card from "@/components/Card";
 
 // icons
 import ConsoleIcon from "@/assets/icons/ConsoleIcon";
 import { Fragment } from "react";
 
 const Genres = () => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const {
+    isLoading,
+    isError,
+    data: platformsData,
+  } = useQuery("platforms", getPlatforms);
 
-    const { isLoading, isError, data: platformsData } = useQuery("platforms", getPlatforms);
-
-    return (
-        <div className="default-section-padding w-[100%]">
-            <div className="flex items-center justify-center gap-x-2 mb-5">
-                <ConsoleIcon />
-                <header className="heading">Platforms</header>
-            </div>
-            <div className="grid grid-cols-1 gap-y-5">
-                {platformsData?.results?.map((data) => (
-                    <div key={data.id} onClick={() => router.push(`platforms/${data.id}`)}>
-                        <Card data={data} />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="default-section-padding w-[100%]">
+      <div className="flex items-center justify-center gap-x-2 mb-5">
+        <ConsoleIcon />
+        <header className="heading">Platforms</header>
+      </div>
+      <div className="grid grid-cols-1 gap-y-5">
+        {platformsData?.results?.map((data) => (
+          <div
+            key={data.id}
+            onClick={() => router.push(`platforms/${data.id}`)}
+          >
+            <Card data={data} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Genres;
