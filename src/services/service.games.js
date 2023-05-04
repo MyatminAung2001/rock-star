0ba286bd6914ff0ea2c0fb1b04b34f19b77fb944
Myat Min Aung <iridiamthegreat@gmail.com>
2023-05-04 23:52:04 +0630

    Update: page size at best of the year api

--- src/services/service.games.js
+++ src/services/service.games.js
@@ -3,9 +3,13 @@ import { apiKEY } from "@/constants/api";
 import { GET_GAMES } from "@/constants/api";
 
 export const getGames = async (pageParam) => {
-    return await axiosInstance.get(`${GET_GAMES}/lists/main?key=${apiKEY}&discover=true&page=${pageParam}`).then(res => res.data);
+    return await axiosInstance.get(`
+        ${GET_GAMES}/lists/main?key=${apiKEY}&discover=true&page=${pageParam}`
+    ).then(res => res.data);
 };
 
 export const getBestOfTheYear = async (pageParam) => {
-    return await axiosInstance.get(`${GET_GAMES}/lists/greatest?key=${apiKEY}&discover=true&page=${pageParam}`).then(res => res.data);
+    return await axiosInstance.get(`
+        ${GET_GAMES}/lists/greatest?key=${apiKEY}&discover=true&page=${pageParam}&page_size=10
+    `).then(res => res.data);
 };
