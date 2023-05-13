3ee6bb970ca9435aff21e69c47a15684037d7adf
MyatMinAung <myatminaung.dev@gmail.com>
2023-05-13 11:57:44 +0630

    Refactor: popular in 2022

---
+++ src/app/popular-in-2022/useContainer.js
@@ -0,0 +1,59 @@
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import { useInfiniteQuery } from "react-query";
+import { useInView } from "react-intersection-observer";
+
+import { getPopularIn2022 } from "@/services/service.games";
+
+const useContainer = () => {
+    const router = useRouter();
+
+    const { ref, inView } = useInView();
+
+    const [page, setPage] = useState(1);
+
+    const {
+        data: PopularIn2022,
+        hasNextPage,
+        fetchNextPage,
+        isFetchingNextPage,
+        isFetching,
+        isLoading,
+        isError,
+    } = useInfiniteQuery(
+        "popular-in-2022",
+        ({ pageParam = 1 }) => getPopularIn2022(pageParam),
+        {
+            getNextPageParam: (lastPage) => {
+                if (lastPage.length === 0) {
+                    return undefined;
+                }
+                return page + 1;
+            },
+            keepPreviousData: true,
+        }
+    );
+
+    useEffect(() => {
+        if (inView && hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+            setPage(page + 1);
+        }
+    }, [inView, hasNextPage, isFetchingNextPage, page, fetchNextPage]);
+
+    const gamesData = (PopularIn2022?.pages || []).flatMap(
+        (page) => page.results || []
+    );
+    const formattedData = gamesData || [];
+
+    return {
+        router,
+        ref,
+        isLoading,
+        isError,
+        isFetchingNextPage,
+        formattedData,
+    };
+};
+
+export default useContainer;
