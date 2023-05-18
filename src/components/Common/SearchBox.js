698c79995bef5fb0570aa0b2060caaa155b12203
MyatMinAung <myatminaung.dev@gmail.com>
2023-05-18 22:22:18 +0630

    Update: search games loading

--- src/components/Common/SearchBox.js
+++ src/components/Common/SearchBox.js
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useQuery, useQueryClient } from "react-query";
 import { LazyLoadImage } from "react-lazy-load-image-component";
@@ -9,6 +9,7 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 import SearchIcon from "./icons/SearchIcon";
 import { getSearchGames } from "@/services/service.games";
 import useDebounce from "@/hooks/useDebounce";
+import { Loading } from "./Loading";
 
 const SearchBox = () => {
     const [searchTerm, setSearchTerm] = useState("");
@@ -41,25 +42,29 @@ const SearchBox = () => {
     console.log("search", searchGames?.results);
 
     return (
-        <div className="mt-[4rem] px-4 lg:mt-3 relative">
+        <div className="mt-[4rem] mx-4 lg:mt-3 relative">
             <div className="flex items-center justify-between w-[100%] lg:w-[500px] px-4 py-2 bg-[#212529] rounded-full">
                 <div className="cursor-pointer">
                     <SearchIcon />
                 </div>
                 <input
-                    type="text"
+                    type="search"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                     placeholder="Search for"
                     className="w-[100%] ml-3 bg-transparent placeholder:text-sm focus:outline-none text-white"
                 />
             </div>
-            {isLoading ? (
-                <p className="text-white text-center">Loading...</p>
-            ) : (
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-2 mt-3">
+            {isLoading && (
+                <div className="absolute z-40 w-[100%] mt-3 ml-3 right-0 flex items-center justify-center h-[100px] bg-[#212529] px-4 py-3 rounded-md">
+                    <Loading />
+                </div>
+            )}
+            {searchGames?.results.length > 0 && (
+                <div className="absolute z-40 w-[100%] right-0 bg-[#212529] px-4 py-3 rounded-md grid grid-cols-1 lg:grid-cols-3 gap-y-2 mt-3">
                     {searchGames?.results.map((data) => (
-                        <div
+                        <Link
+                            href={`/games/${data.slug}`}
                             key={data.id}
                             className="flex items-start gap-x-2 relative"
                         >
@@ -86,7 +91,7 @@ const SearchBox = () => {
                                     {data.name}
                                 </p>
                             </div>
-                        </div>
+                        </Link>
                     ))}
                 </div>
             )}
