2ac1725977348d888ef65016dc3509bf7dd7bf16
MyatMinAung <myatminaung.dev@gmail.com>
2023-05-20 18:16:31 +0630

    Update: useQueries

--- src/app/page.js
+++ src/app/page.js
@@ -1,86 +1,17 @@
 "use client";
 
-import Link from "next/link";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-
 import useContainer from "./useContainer";
-import { NUMBER_OF_GENRES } from "@/constants/restrict";
 import { Loading } from "@/components/Common/Loading";
+import SwiperCard from "@/components/Home/SwiperCard";
 
 const Page = () => {
-    const { UpcomingGames, isError, isLoading } = useContainer();
+    const { UpcomingGames, isLoading } = useContainer();
 
     if (isLoading) return <Loading />;
 
     return (
         <div className="mb-5 xl:w-[75rem] 2xl:w-[105rem]">
-            <div className="default-section-padding">
-                <p className="heading mb-3">Upcoming Release</p>
-                <Swiper
-                    className="mySwiper games-card"
-                    spaceBetween={10}
-                    slidesPerView={"auto"}
-                >
-                    {UpcomingGames?.results.map((data) => (
-                        <SwiperSlide key={data.id} style={{ width: "auto" }}>
-                            <div className="bg-[#212529] rounded-xl flex flex-col h-full">
-                                <LazyLoadImage
-                                    src={data.background_image}
-                                    alt="screenshot"
-                                    effect="blur"
-                                    threshold={50}
-                                    className="object-cover w-[300px] lg:w-[370px] h-[180px] lg:h-[230px] rounded-t-lg"
-                                />
-                                <Link
-                                    href={`/games/${data.slug}`}
-                                    className="pt-2 px-4 w-[300px] lg:w-[370px] text-primary-white text-xl font-semibold line-clamp-2 transition hover:text-primary-yellow duration-300 cursor-pointer"
-                                >
-                                    {data.name}
-                                </Link>
-                                <div className="px-4 pb-2 mt-auto flex flex-col">
-                                    <div className="mb-2 flex items-center justify-between">
-                                        <p className="text-secondary-gray text-sm">
-                                            Release date
-                                        </p>
-                                        <p className="text-primary-white text-sm font-light">
-                                            {new Date(
-                                                data.released
-                                            ).toLocaleDateString("en-us", {
-                                                year: "numeric",
-                                                month: "long",
-                                                day: "numeric",
-                                            })}
-                                        </p>
-                                    </div>
-
-                                    <div className="mb-2 bg-primary-bg-grey w-[100%] h-[0.5px]" />
-
-                                    <div className="flex items-center justify-between">
-                                        <p className="text-secondary-gray text-sm">
-                                            Genres
-                                        </p>
-                                        <div className="flex items-center gap-x-1">
-                                            {data?.genres
-                                                ?.slice(0, NUMBER_OF_GENRES)
-                                                .map((data) => (
-                                                    <p
-                                                        key={data.id}
-                                                        className="text-primary-white text-sm font-light underline"
-                                                    >
-                                                        {data.name}
-                                                    </p>
-                                                ))}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
+            <SwiperCard upcoming={UpcomingGames?.results} />
         </div>
     );
 };
--- src/app/useContainer.js
+++ src/app/useContainer.js
@@ -1,20 +1,23 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 
 import { getUpcomingGames } from "@/services/service.games";
 
 const useContainer = () => {
-    const {
-        data: UpcomingGames,
-        isError,
-        isLoading,
-    } = useQuery({
-        queryKey: ["upcoming"],
-        queryFn: () => getUpcomingGames(),
+    const queryResults = useQueries({
+        queries: [
+            { queryKey: ["upcoming"], queryFn: () => getUpcomingGames() },
+        ],
     });
+
+    const UpcomingGames = queryResults[0].data;
+
+    console.log("upcoming", UpcomingGames);
+
+    // loading
+    const isLoading = queryResults.some((result) => result.isLoading);
 
     return {
         UpcomingGames,
-        isError,
         isLoading,
     };
 };
