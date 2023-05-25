551765356d60b2a09e818a0c0fb709e23a457d12
MyatMinAung <myatminaung.dev@gmail.com>
2023-05-25 22:57:36 +0630

    :children_crossing: Update: query state in all time top 250

--- src/app/all-time-top-250/index.js
+++ src/app/all-time-top-250/index.js
@@ -6,26 +6,37 @@ import useContainer from "./useContainer";
 
 const AllTimeTop250 = () => {
     const {
-        router,
         ref,
         isLoading,
         isError,
         isFetchingNextPage,
+        hasNextPage,
         formattedData,
     } = useContainer();
 
     if (isLoading) return <Loading />;
 
+    if (isError) return <p>Error...</p>;
+
     return (
         <div className="default-section-padding">
             <p className="heading mb-3">All time top 250</p>
+
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-5">
                 {formattedData?.map((data) => (
                     <GameCard key={data.id} data={data} />
                 ))}
             </div>
 
-            <div ref={ref}>{isFetchingNextPage && <FetchingNextPage />}</div>
+            <div ref={ref}>
+                {hasNextPage ? (
+                    isFetchingNextPage && <FetchingNextPage />
+                ) : (
+                    <p className="text-white text-center mt-3">
+                        No More Results
+                    </p>
+                )}
+            </div>
         </div>
     );
 };
