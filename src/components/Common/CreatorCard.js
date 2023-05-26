db82ae2fcb6bd288ab2c9e34f9904f4b30ae186e
MyatMinAung <myatminaung.dev@gmail.com>
2023-05-26 21:39:25 +0630

    :lipstick: update: creator card ui

--- src/components/Common/CreatorCard.js
+++ src/components/Common/CreatorCard.js
@@ -4,76 +4,49 @@ import { NUMBER_OF_ITEMS } from "@/constants/restrict";
 
 const CreatorCard = ({ data }) => {
     return (
-        <div
-            key={data.id}
-            className="w-[100%] h-[320px] flex items-center justify-center relative"
-        >
-            <span
-                style={{
-                    content: "",
-                    backgroundImage: `
-                    linear-gradient(rgba(32, 32, 32, 0.5), rgb(32, 32, 32) 70%),
-                    url(${data.image_background})
-                `,
-                    backgroundSize: "cover",
-                    backgroundPosition: "50%",
-                    position: "absolute",
-                    inset: "0px",
-                    borderRadius: "0.5rem",
-                    backdropFilter: "blur(10px)",
-                }}
+        <div key={data.id} className="w-[100%] bg-card-bg-color rounded-lg">
+            <Image
+                src={data.image_background}
+                alt="background"
+                width={300}
+                height={300}
+                className="object-cover w-full h-[230px] rounded-t-lg"
             />
-            <div className="w-[100%] px-6 py-3 relative h-full flex flex-col justify-between bg-[#00000030] rounded-lg">
-                <div className="flex flex-col items-center justify-center">
-                    {data?.image && (
-                        <Image
-                            src={data?.image}
-                            alt={data?.name}
-                            width={80}
-                            height={80}
-                            className="rounded-full mb-3"
-                        />
-                    )}
-                    <p className="mb-3 text-white text-base font-semibold tracking-wider text-center underline cursor-pointer">
-                        {data.name}
-                    </p>
+            <div className="px-4 py-2 flex items-center justify-start gap-x-4">
+                {data?.image && (
+                    <Image
+                        src={data?.image}
+                        alt={data?.name}
+                        width={70}
+                        height={70}
+                        className="mb-3 w-[70px] h-[70px] rounded-full"
+                    />
+                )}
+                <div>
+                    <div className="mb-2 flex items-baseline gap-x-3">
+                        <p className="text-base font-semibold tracking-wider text-primary-white">
+                            {data.name}:
+                        </p>
+                        <p className="text-primary-white text-sm">
+                            {data.games_count} Games
+                        </p>
+                    </div>
+                    <div className="divider" />
                     {data?.positions && (
-                        <div className="flex items-center justify-center gap-x-2">
+                        <div className="mb-1 flex items-center justify-start gap-x-2">
                             {data?.positions.map((data, i, arr) => (
                                 <p
                                     key={data.id}
-                                    className="text-white text-xs first-letter:uppercase"
+                                    className="text-white text-sm first-letter:uppercase"
                                 >
                                     {data.name}
-                                    {i != arr.length - 1 ? ", " : " "}
+                                    <span className="text-primary-yellow">
+                                        {i !== arr.length - 1 ? " | " : " "}
+                                    </span>
                                 </p>
                             ))}
                         </div>
                     )}
-                </div>
-                <div>
-                    <div className="flex items-center justify-between">
-                        <p className="text-secondary-white text-sm font-semibold">
-                            Known For:
-                        </p>
-                        <p className="text-secondary-gray text-sm">
-                            {data.games_count}
-                        </p>
-                    </div>
-                    <hr className="my-3" />
-                    {data.games?.slice(0, NUMBER_OF_ITEMS)?.map((data) => (
-                        <div
-                            key={data.id}
-                            className="flex items-center justify-between gap-y-1"
-                        >
-                            <p className="text-secondary-white font-light underline text-sm">
-                                {data.name}
-                            </p>
-                            <p className="text-secondary-gray text-sm">
-                                {data.added}
-                            </p>
-                        </div>
-                    ))}
                 </div>
             </div>
         </div>
