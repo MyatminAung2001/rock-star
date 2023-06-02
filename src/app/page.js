4b24a3b52e7088ae05d9001d379f9b851aca7a65
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-02 22:58:21 +0630

    Refactor: home page structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,7 @@
-"use client";
-
-import useContainer from "./useContainer";
-import { Loading } from "@/components/Common/Loading";
-import SwiperCard from "@/components/Home/SwiperCard";
+import Home from "./home";
 
 const Page = () => {
-    const {
-        isLoading,
-        UpcomingGames,
-        Last30DaysGames,
-        ThisWeekGames,
-        NextWeekGames,
-    } = useContainer();
-
-    if (isLoading) return <Loading />;
-
-    return (
-        <div className="mb-5 xl:w-[73rem] 2xl:w-[105rem]">
-            <div className="default-section-padding">
-                <p className="heading mb-3">Upcoming Games</p>
-                <SwiperCard gamesData={UpcomingGames?.results} />
-            </div>
-
-            <div className="default-section-padding">
-                <p className="heading mb-3">Last 30 Days</p>
-                <SwiperCard gamesData={Last30DaysGames?.results} />
-            </div>
-
-            <div className="default-section-padding">
-                <p className="heading mb-3">This Week</p>
-                <SwiperCard gamesData={ThisWeekGames?.results} />
-            </div>
-
-            <div className="default-section-padding">
-                <p className="heading mb-3">Next Week</p>
-                <SwiperCard gamesData={NextWeekGames?.results} />
-            </div>
-        </div>
-    );
+    return <Home />;
 };
 
 export default Page;
