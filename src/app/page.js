43fbbfd46b4947a5e18f5c35ec5df6912f94e934
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-02 23:13:26 +0630

    Update: prefetch home page api

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,37 @@
+import { dehydrate } from "@tanstack/react-query";
+
+import getQueryClient from "@/utils/getQueryCilent";
+import Hydrate from "@/utils/hydrateOnClient";
+import {
+    getLast30Days,
+    getNextWeek,
+    getThisWeek,
+    getUpcomingGames,
+} from "@/services/service.games";
 import Home from "./home";
 
-const Page = () => {
-    return <Home />;
-};
+export default async function Page() {
+    const queryClient = getQueryClient();
 
-export default Page;
+    const queryConfig = {
+        upcoming: () => getUpcomingGames(),
+        "last-30days": () => getLast30Days(),
+        "this-week": () => getThisWeek(),
+        "next-week": () => getNextWeek(),
+    };
+
+    const prefetchPromises = Object.entries(queryConfig).map(
+        ([queryKey, fetchDataFn]) =>
+            queryClient.prefetchQuery(queryKey, fetchDataFn)
+    );
+
+    await Promise.all(prefetchPromises);
+
+    const dehydrateState = dehydrate(queryClient);
+
+    return (
+        <Hydrate state={dehydrateState}>
+            <Home />
+        </Hydrate>
+    );
+}
