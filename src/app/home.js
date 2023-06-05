878b331c83a04bc4284135092b3e70e8a39c0de4
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-05 20:32:00 +0630

    Update: home page section padding

--- src/app/home.js
+++ src/app/home.js
@@ -17,22 +17,22 @@ const Home = () => {
 
     return (
         <div className="mb-5 xl:w-[73rem] 2xl:w-[105rem]">
-            <div className="default-section-padding">
+            <div className="p-4">
                 <p className="heading mb-3">Upcoming Games</p>
                 <SwiperCard gamesData={UpcomingGames?.results} />
             </div>
 
-            <div className="default-section-padding">
+            <div className="p-4">
                 <p className="heading mb-3">Last 30 Days</p>
                 <SwiperCard gamesData={Last30DaysGames?.results} />
             </div>
 
-            <div className="default-section-padding">
+            <div className="p-4">
                 <p className="heading mb-3">This Week</p>
                 <SwiperCard gamesData={ThisWeekGames?.results} />
             </div>
 
-            <div className="default-section-padding">
+            <div className="p-4">
                 <p className="heading mb-3">Next Week</p>
                 <SwiperCard gamesData={NextWeekGames?.results} />
             </div>
