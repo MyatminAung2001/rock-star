eb8a9b3951e24bbde21122b1b0f963bd99611882
MyatMinAung <myatminaung.dev@gmail.com>
2023-07-17 22:11:00 +0630

    fix: query provider children node

--- src/utils/reactquery.provider.tsx
+++ src/utils/reactquery.provider.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export const ReactQueryProviders = ({ children }) => {
+export const ReactQueryProviders = ({ children }: { children: ReactNode }) => {
     const [queryClient] = useState(
         () =>
             new QueryClient({
